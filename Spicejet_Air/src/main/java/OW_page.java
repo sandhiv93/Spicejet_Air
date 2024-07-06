7b609082097c5f2cadc8cba12ccf3f21f188446a
sandhiv93 <167868273+sandhiv93@users.noreply.github.com>
2024-07-06 15:23:24 +0530

    Add files via upload

---
+++ Spicejet_Air/src/main/java/OW_page.java
@@ -0,0 +1,209 @@
+package com.spicejet.pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import com.spicejet.utils.SeWrappers;
+
+public class OW_Page extends SeWrappers{
+
+    //1. In Main page click Oneway option
+    @FindBy(xpath = "//div[contains(text(),'one way')]")
+    public WebElement onewayradio;
+
+    public void clickonewaybutton() {
+        setImplicitWait(15);
+        clickElement(onewayradio);
+    }
+
+    //////////////////////////////////////////////////////////
+
+    //2. In Main page click FROM place
+    @FindBy(xpath = "//body/div[@id='react-root']/div[@id='main-container']/div[1]/div[1]/div[3]/div[2]/div[3]/div[1]/div[1]/div[1]/div[2]/input[1]")
+    public WebElement from;
+
+    public void enterFromPlace(String place1) {
+        //waitforelementExplicitwait(from, 15);
+        clickElement(from);
+        typeText(from, place1);
+    }
+
+    //////////////////////////////////////////////////////////
+
+    //3. In Main page click TO place
+    @FindBy(xpath = "//div[@data-testid='to-testID-destination']//input[@type='text']")
+    public WebElement to;
+
+    public void enterToPlace(String place2) {
+        waitforelementExplicitwait(to, 20);
+        clickElement(to);
+        typeText(to, place2);
+    }
+
+    //////////////////////////////////////////////////////////
+
+    //4. In Main page pick depature date
+    @FindBy(xpath = "//div[text()='Departure Date']")
+    public WebElement departureDate;
+    public void selectDepaDate() {
+        waitforelementExplicitwait(departureDate, 15);
+        actionsDoubleclickElement(departureDate);
+    }
+
+    @FindBy(xpath = "(//div[@class='css-76zvg2 r-homxoj r-ubezar r-16dba41'])[63]")
+    //Feb02
+    public WebElement reqdate;
+    public void selectReqDate() {
+        waitforelementExplicitwait(reqdate, 10);
+        clickElement(reqdate);
+    }
+
+    //////////////////////////////////////////////////////////
+
+    //5. In Main page clicksearchFlight
+    @FindBy(xpath = "//*[@id=\"main-container\"]/div/div[1]/div[3]/div[2]/div[7]/div[2]/div")
+    public WebElement searchFlights;
+
+    public void clicksearchFlight() throws InterruptedException {
+        //waitforelementExplicitwait(searchFlights, 15);
+        clickElement(searchFlights);
+    }
+
+    //////////////////////////////////////////////////////////
+
+    //6. Next page click continue button
+    @FindBy(xpath = "//*[@id=\"main-container\"]/div/div[1]/div[5]/div/div/div[3]/div[2]")
+    public WebElement nxtpgcontinueButton;
+
+    public void clickcontinueButton() throws InterruptedException {
+        waitforelementExplicitwait(nxtpgcontinueButton, 5);
+        clickElement(nxtpgcontinueButton);
+    }
+
+    //////////////////////////////////////////////////////////
+
+    //7. Next page Enter personal details
+    @FindBy(xpath = "(//div[@class='css-1dbjc4n r-1awozwy r-18u37iz r-1wtj0ep'])[1]")
+    public WebElement title;
+    public void clicktitle(String value) {
+        clickElement(title);
+        selectByValueDropDown(title, value);
+    }
+
+    @FindBy(xpath = "//*[@id=\"primary-contact-details\"]/div[3]/div[2]/div/div/div[2]/input")
+    public WebElement Fnameclear;
+    public void clearFirstname() {
+        actionsDoubleclickElement(Fnameclear);
+        selectText(Fnameclear);
+        waitforelementFluentwait(Fnameclear, 10, 20);
+        deleteText(Fnameclear);
+    }
+    @FindBy(xpath = "//*[@id=\"primary-contact-details\"]/div[3]/div[2]/div/div/div[2]/input")
+    public WebElement Fname;
+    public void enterFname(String fname) throws InterruptedException {
+        clickElement(Fname);
+        waitforelementExplicitwait(Fname, 20);
+        typeText(Fname, fname);
+    }
+
+    @FindBy(xpath = "//*[@id=\"primary-contact-details\"]/div[3]/div[3]/div/div/div[2]/input")
+    public WebElement Lnameclear;
+    public void clearLastname() {
+        actionsDoubleclickElement(Lnameclear);
+        selectText(Lnameclear);
+        waitforelementFluentwait(Lnameclear, 10, 20);
+        deleteText(Lnameclear);
+    }
+
+    @FindBy(xpath = "//*[@id=\"primary-contact-details\"]/div[3]/div[3]/div/div/div[2]/input")
+    public WebElement Lname;
+    public void enterLname(String lname) throws InterruptedException {
+        clickElement(Lname);
+        waitforelementExplicitwait(Lname, 20);
+        typeText(Lname, lname);
+    }
+
+    @FindBy(xpath = "//*[@data-testid='contact-number-input-box']")
+    public WebElement contactboxclear;
+    public void clearcontactBox() {
+        actionsDoubleclickElement(contactboxclear);
+        selectText(contactboxclear);
+        waitforelementFluentwait(contactboxclear, 10, 20);
+        deleteText(contactboxclear);
+    }
+    @FindBy(xpath = "//*[@data-testid='contact-number-input-box']")
+    public WebElement contactbox;
+    public void entercontactNumber(String number) throws InterruptedException {
+        clickElement(contactbox);
+        waitforelementExplicitwait(contactbox, 20);
+        typeText(contactbox, number);
+    }
+
+    @FindBy(xpath = "//*[@id=\"primary-contact-details\"]/div[4]/div[1]/div/div/div[2]/input")
+    public WebElement mailidclear;
+    public void clearmailid() {
+        actionsDoubleclickElement(mailidclear);
+        selectText(mailidclear);
+        waitforelementFluentwait(mailidclear, 10, 20);
+        deleteText(mailidclear);
+    }
+    @FindBy(xpath = "//*[@id=\"primary-contact-details\"]/div[4]/div[1]/div/div/div[2]/input")
+    public WebElement mailid;
+    public void enteremailid(String email) throws InterruptedException {
+        clickElement(mailid);
+        waitforelementExplicitwait(mailid, 20);
+        typeText(mailid , email);
+    }
+
+    @FindBy(xpath = "//*[@id=\"primary-contact-details\"]/div[4]/div[3]/div/div/div[2]/input")
+    public WebElement townname;
+    public void entertownName(String city) throws InterruptedException {
+        clickElement(townname);
+        Thread.sleep(3000);
+        typeText(townname, city);
+    }
+
+    @FindBy(xpath = "//div[@class='css-1dbjc4n']//div[@class='css-1dbjc4n r-1awozwy r-1loqt21 r-18u37iz r-1otgn73']//div[@class='css-1dbjc4n r-zso239']")
+    public WebElement checkBox;
+    public void clickcheckBox() {
+        clickElement(checkBox);
+    }
+
+    //////////////////////////////////////////////////////////
+
+    //8.Click continue button in Main page
+    @FindBy(xpath = "//*[@id=\"travellers-view\"]/div[2]/div/div/div[2]")
+    public WebElement mainContinuebutton;
+
+    public void clickContinue() throws InterruptedException {
+        setImplicitWait(15);
+        clickElement(mainContinuebutton);
+        Thread.sleep(3000);
+    }
+
+    //////////////////////////////////////////////////////////
+
+    //9.Click again continue button in next Main page
+    @FindBy(xpath = "//body/div[@id='react-root']/div[@id='main-container']/div[1]/div[5]/div[1]/div[1]/div[2]/div[1]/div[1]/div[4]")
+    public WebElement main2Continuebutton;
+
+    public void clickContinue2() throws InterruptedException {
+        setImplicitWait(15);
+        clickElement(main2Continuebutton);
+        Thread.sleep(3000);
+    }
+
+    //////////////////////////////////////////////////////////
+
+    //10.Skip the seat selecting from popupwindow
+    @FindBy(xpath = "//span[text()='Skip this to skip comfort.']")
+    public WebElement skipskip;
+
+    public void clickSkipSkip()throws InterruptedException  {
+        Thread.sleep(5000);
+        //waitforelementExplicitwait(skipskip, 20);
+        setImplicitWait(15);
+        clickElement(skipskip);
+    }
+
+}
